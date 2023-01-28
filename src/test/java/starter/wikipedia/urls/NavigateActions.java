2a5bcb8618b0e9ad0e25a262dbd53fd06d97bb41
Martin Blazek <martin.blazek@ness.com>
2023-01-28 09:07:58 +0100

    enviroment variables removed

--- src/test/java/starter/wikipedia/urls/NavigateActions.java
+++ src/test/java/starter/wikipedia/urls/NavigateActions.java
@@ -2,13 +2,13 @@ package starter.wikipedia.urls;
 
 import net.serenitybdd.core.steps.UIInteractions;
 import net.thucydides.core.annotations.Step;
-import org.junit.jupiter.api.Tag;
 
 
 public class NavigateActions extends UIInteractions {
 
     @Step("Navigate to the home page")
     public void toTheHomePage() {
-        openUrl( System.getenv("master").equals("false") ? "https://martinpersonalweb.vercel.app/" : "http://localhost:3000/");
+        //openUrl( System.getenv("master").equals("false") ? "https://martinpersonalweb.vercel.app/" : "http://localhost:3000/");
+        openUrl("https://martinpersonalweb.vercel.app/");
     }
 }
